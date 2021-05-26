425a060c01cc214bc786f181c95a00a4fc63c54b
Andrew Jensen <andrewjensen90@gmail.com>
2021-05-25 20:22:31 -0600

    Stub out a few color-related blocks

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,21 +1,107 @@
-
-import { BlocklyWorkspace } from 'react-blockly';
+import Blockly, { Block } from 'blockly';
+import { BlocklyEditor } from 'react-blockly';
 
 import './App.css';
 
+interface ToolboxCategory {
+  name: string,
+  custom?: CustomToolboxCategory,
+  blocks: ToolboxBlock[],
+}
+
+interface ToolboxBlock {
+  type: string,
+  colour?: string,
+  // TODO: fill in with details from here:
+  // https://github.com/nbudin/react-blockly/blob/v6-stable/README.md
+}
+
+type CustomToolboxCategory = 'VARIABLE' | 'PROCEDURE';
+
+const TOOLBOX_CATEGORIES: ToolboxCategory[] = [
+  {
+    name: 'Colors',
+    blocks: [
+      { type: 'start' },
+      { type: 'set_color' },
+      { type: 'light_on' },
+      { type: 'light_off' },
+    ]
+  }
+];
+
 const INITIAL_XML = `
-  <xml xmlns="http://www.w3.org/1999/xhtml">
-    <block type="text" x="70" y="30">
-      <field name="TEXT">Code Gets Creative!</field>
-    </block>
-  </xml>
+  <xml xmlns="http://www.w3.org/1999/xhtml"></xml>
 `;
+
+Blockly.Blocks['start'] = {
+  init: function() {
+    let block = this as Block;
+
+    block.appendDummyInput()
+        .appendField("when the program starts, do:");
+    block.setNextStatement(true, null);
+    block.setColour(230);
+    block.setTooltip("");
+    block.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['set_color'] = {
+  init: function() {
+    let block = this as Block;
+    block.appendDummyInput()
+        .appendField("set color to");
+    block.appendValueInput("H")
+        .setCheck(null)
+        .appendField("H");
+    block.appendValueInput("S")
+        .setCheck(null)
+        .appendField("S");
+    block.appendValueInput("B")
+        .setCheck(null)
+        .appendField("B");
+    block.setInputsInline(true);
+    block.setPreviousStatement(true, null);
+    block.setNextStatement(true, null);
+    block.setColour(230);
+    block.setTooltip("");
+    block.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['light_on'] = {
+  init: function() {
+    let block = this as Block;
+    block.appendDummyInput()
+        .appendField("turn the light on");
+    block.setPreviousStatement(true, null);
+    block.setNextStatement(true, null);
+    block.setColour(230);
+    block.setTooltip("");
+    block.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['light_off'] = {
+  init: function() {
+    let block = this as Block;
+    block.appendDummyInput()
+        .appendField("turn the light off");
+    block.setPreviousStatement(true, null);
+    block.setNextStatement(true, null);
+    block.setColour(230);
+    block.setTooltip("");
+    block.setHelpUrl("");
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <BlocklyWorkspace
+      <BlocklyEditor
         wrapperDivClassName="fill-height"
+        toolboxCategories={TOOLBOX_CATEGORIES}
         workspaceConfiguration={{
           grid: {
             spacing: 20,
--- ui/src/libraries.d.ts
+++ ui/src/libraries.d.ts
@@ -1,3 +1,3 @@
 declare module 'react-blockly' {
-  declare var BlocklyWorkspace: any;
+  declare var BlocklyEditor: any;
 }
