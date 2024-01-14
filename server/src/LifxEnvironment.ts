3ef659140ef17db8f477c01761d83ca6c7f73211
Andrew Jensen <andrewjensen90@gmail.com>
2024-01-13 17:56:56 -0700

    Get reachable lights, turn lights on and off

--- server/src/LifxEnvironment.ts
+++ server/src/LifxEnvironment.ts
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 import { clamp, modulo } from './mathUtils';
 import { pause } from './timingUtils';
 import logger from './logger';
@@ -5,29 +7,78 @@ import { LightEnvironment } from './types';
 
 const DEFAULT_TRANSITION_TIME_MS = 1000;
 
+const SELECTOR_ALL = 'all';
+
 export default class LifxEnvironment implements LightEnvironment {
+  private numericIdsToLifxIds: Record<number, string>;
+  private reachableIds: number[];
+
   constructor() {
+    this.numericIdsToLifxIds = {};
+    this.reachableIds = [];
   }
 
   async initialize() {
     logger.info('Initializing LIFX light environment...');
 
-    // TODO: implement
+    const lights = await getAllLights();
+    for (let idx = 0; idx < lights.length; idx++) {
+      const numericId = idx + 1;
+      const light = lights[idx];
+      const lifxId = light.id;
+
+      this.numericIdsToLifxIds[numericId] = lifxId;
+
+      if (light.connected) {
+        this.reachableIds.push(numericId);
+      }
+    }
   }
 
   getReachableLightIds(): number[] {
-    // TODO: implement
-    return [];
+    return this.reachableIds;
   }
 
   async lightOn(lightId: number | null): Promise<unknown> {
-    // TODO: implement
-    return Promise.resolve();
+    if (lightId) {
+      const lifxId = this.numericIdsToLifxIds[lightId];
+      return Promise.all([
+        setLightState(lifxId, {
+          isOn: true,
+          durationMs: DEFAULT_TRANSITION_TIME_MS,
+        }),
+        pause(DEFAULT_TRANSITION_TIME_MS),
+      ]);
+    } else {
+      return Promise.all([
+        setLightState(SELECTOR_ALL, {
+          isOn: true,
+          durationMs: DEFAULT_TRANSITION_TIME_MS,
+        }),
+        pause(DEFAULT_TRANSITION_TIME_MS),
+      ]);
+    }
   }
 
   async lightOff(lightId: number | null): Promise<unknown> {
-    // TODO: implement
-    return Promise.resolve();
+    if (lightId) {
+      const lifxId = this.numericIdsToLifxIds[lightId];
+      return Promise.all([
+        setLightState(lifxId, {
+          isOn: false,
+          durationMs: DEFAULT_TRANSITION_TIME_MS,
+        }),
+        pause(DEFAULT_TRANSITION_TIME_MS),
+      ]);
+    } else {
+      return Promise.all([
+        setLightState(SELECTOR_ALL, {
+          isOn: false,
+          durationMs: DEFAULT_TRANSITION_TIME_MS,
+        }),
+        pause(DEFAULT_TRANSITION_TIME_MS),
+      ]);
+    }
   }
 
   async setColor(lightId: number | null, hue: number, saturation: number, brightness: number) {
@@ -42,4 +93,40 @@ export default class LifxEnvironment implements LightEnvironment {
     // TODO: implement
     return Promise.resolve();
   }
+}
+
+async function getAllLights() {
+  const response = await fetch('https://api.lifx.com/v1/lights/all', {
+    headers: {
+      'Authorization': `Bearer ${process.env.LIFX_API_TOKEN}`,
+    },
+  });
+  const responseJson = await response.json();
+
+  return responseJson;
+}
+
+type TransitionOptions = {
+  isOn: boolean;
+  durationMs: number;
+}
+
+async function setLightState(selector: string, options: TransitionOptions) {
+  const requestBody = {
+    power: options.isOn ? 'on' : 'off',
+    // TODO: handle duration
+    // duration: options.durationMs / 1000,
+  };
+
+  const response = await fetch(`https://api.lifx.com/v1/lights/${selector}/state`, {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${process.env.LIFX_API_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestBody),
+  });
+  const responseJson = await response.json();
+
+  return responseJson;
 }
